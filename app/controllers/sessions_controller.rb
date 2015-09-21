9b6bc9ea9e43043900693e7071c90cc038bde11c
Tom <tom_t_lee@yahoo.com>
2015-09-21 17:11:57 +0000

    Add controller and routes for handling OAuth callbacks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+
+    @user = User.find_or_create_by!(remote_id: auth.uid, provider: "github") do |user|
+      user.name = auth.info.nickname
+      user.token = auth.credentials.token
+    end
+
+    session[:user_id] = @user.id
+    flash[:info] = "You have signed in with GitHub."
+    redirect_to root_path
+  end
+
+  def failure
+    flash[:danger] = "Authorization failed."
+    redirect_to root_path
+  end
 end
